559f9fa74fe327e44278508448a94e3443ed01ee
dnabilaa <70683312+dnabilaa@users.noreply.github.com>
2020-09-28 23:04:53 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,26 @@
-
+<?php
+if ($_GET):
+    $a = (double) @$_GET['a'];
+    $b = (double) @$_GET['b'];
+    $operator = @$_GET['operator'];
+    
+    switch ($operator) {
+        case '+':
+            $hasil = $a + $b;
+            break;
+        case '-':
+            $hasil = $a - $b;
+            break;
+        case '*':
+            $hasil = $a * $b;
+            break;
+        case '/':
+            $hasil = $a / $b;
+            break;
+    }
+    ?>
+    <div style="margin-top: 1rem">
+    Hasil: <strong><?php echo $hasil ?></strong>
+    </div>
+    <?php
+endif; ?>
